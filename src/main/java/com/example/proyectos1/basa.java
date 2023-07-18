c47e230731b957c414ec5d6b32494c5e430b2a3e
MiguelMoya89 <127111987+MiguelMoya89@users.noreply.github.com>
2023-07-18 18:51:42 +0200

    Actualizacion 3.0

---
+++ src/main/java/com/example/proyectos1/basa.java
@@ -0,0 +1,16 @@
+package com.example.proyectos1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class basa extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
